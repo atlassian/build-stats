5c6941c548830c6fbf1f74137929eb5b48809674
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2020-02-09 06:48:11 +1100

    added support for bamboo to download all the builds at once, this it to overcome the issue where old data for bamboo builds is deleted

--- lib/adapters/index.ts
+++ lib/adapters/index.ts
@@ -1,7 +1,9 @@
 import { default as bitbucket } from './bitbucket';
 import { default as travis } from './travis';
+import { default as bamboo } from './bamboo';
 
 export default {
+  bamboo,
   bitbucket,
   travis
 }
